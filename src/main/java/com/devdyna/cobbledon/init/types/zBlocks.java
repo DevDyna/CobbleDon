d68470cddbf8fdc2117eae2b50d75cf6b9624227
DevDyna <andrearisorti2003@gmail.com>
2025-12-04 21:06:28 +0100

    Update zBlocks.java

--- src/main/java/com/devdyna/cobbledon/init/types/zBlocks.java
+++ src/main/java/com/devdyna/cobbledon/init/types/zBlocks.java
@@ -6,21 +6,27 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.List;
+
+import com.cobblemon.mod.common.CobblemonItems;
 import com.devdyna.cobbledon.Main;
 import com.devdyna.cobbledon.init.Material;
 import com.devdyna.cobbledon.init.builder.PokeLootBall;
+
 @SuppressWarnings("null")
 public class zBlocks {
-        
+
         public static void register(IEventBus bus) {
                 registerLists();
                 zBlock.register(bus);
                 zBlockItem.register(bus);
+                zPokeBall.register(bus);
         }
 
         // ---------------------------------------------------------------------------------------//
         public static final DeferredRegister.Blocks zBlock = DeferredRegister.createBlocks(Main.MODID);
         public static final DeferredRegister.Blocks zBlockItem = DeferredRegister.createBlocks(Main.MODID);
+        public static final DeferredRegister.Blocks zPokeBall = DeferredRegister.createBlocks(Main.MODID);
         // ---------------------------------------------------------------------------------------//
 
         // public static final DeferredHolder<Block, ?> BK = zBlock.register("bk",() ->
@@ -28,13 +34,23 @@ public class zBlocks {
 
         // ---------------------------------------------------------------------------------------//
 
-        public static final DeferredHolder<Block,?> CLASSIC_POKEBALL = Material.registerItemBlock("poke", () -> new PokeLootBall(Properties.of()));
+        public static final DeferredHolder<Block, ?> POKE_BALL = Material.registerItemBlock(
+                        "poke_ball",
+                        () -> new PokeLootBall(Properties.of()), zPokeBall);
+        public static final DeferredHolder<Block, ?> GREAT_BALL = Material.registerItemBlock(
+                        "great_ball",
+                        () -> new PokeLootBall(Properties.of()), zPokeBall);
+        public static final DeferredHolder<Block, ?> ULTRA_BALL = Material.registerItemBlock(
+                        "ultra_ball",
+                        () -> new PokeLootBall(Properties.of()), zPokeBall);
+        public static final DeferredHolder<Block, ?> MASTER_BALL = Material.registerItemBlock(
+                        "master_ball",
+                        () -> new PokeLootBall(Properties.of()), zPokeBall);
+
+        // public static List<DeferredHolder<Block,? extends Block>> values = List.of(POKE_BALL,GREAT_BALL,ULTRA_BALL,MASTER_BALL);
 
 
-        
         public static void registerLists() {
-
-                // List.of(...).forEach(p -> ??);
 
         }
 
