8ff7434332ceb01f018e6bc46206092a6e143dc8
DevDyna <andrearisorti2003@gmail.com>
2025-12-04 21:06:22 +0100

    Update PokeLootBall.java

--- src/main/java/com/devdyna/cobbledon/init/builder/PokeLootBall.java
+++ src/main/java/com/devdyna/cobbledon/init/builder/PokeLootBall.java
@@ -13,15 +13,16 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.neoforged.neoforge.items.ItemHandlerHelper;
 
 @SuppressWarnings("null")
 public class PokeLootBall extends DirectionalBlock {
@@ -34,17 +35,17 @@ public class PokeLootBall extends DirectionalBlock {
     @Override
     @Nullable
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return defaultBlockState().setValue(FACING, Direction.NORTH);
+        return defaultBlockState().setValue(BlockStateProperties.HORIZONTAL_FACING, context.getHorizontalDirection().getOpposite());
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> b) {
-        b.add(FACING);
+        b.add(BlockStateProperties.HORIZONTAL_FACING);
     }
 
     @Override
     protected VoxelShape getShape(BlockState s, BlockGetter l, BlockPos p, CollisionContext c) {
-        return Block.box(2, 0, 2, 14, 12, 14);
+        return Block.box(4, 0, 4, 12, 8, 12);
     }
 
     @Override
@@ -55,9 +56,17 @@ public class PokeLootBall extends DirectionalBlock {
     @Override
     protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player,
             BlockHitResult hitResult) {
-        Block.getDrops(state, (ServerLevel) level, pos, null);
-        level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
-        return InteractionResult.sidedSuccess(level.isClientSide);
+
+        if (!level.isClientSide) {
+            level.removeBlock(pos, false);
+            var drops = Block.getDrops(state, (ServerLevel) level, pos, null);
+            drops.forEach(i -> {
+                ItemHandlerHelper.giveItemToPlayer(player, i);
+            });
+
+            return InteractionResult.SUCCESS;
+        } else
+            return super.useWithoutItem(state, level, pos, player, hitResult);
     }
 
 }
